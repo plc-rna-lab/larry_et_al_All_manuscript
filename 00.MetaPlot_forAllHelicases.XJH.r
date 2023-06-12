77e610ed8822e5bc2d3ff885a77de4c5ffbf6e6f
JingheXie <136046035+JingheXie@users.noreply.github.com>
2023-06-12 17:01:00 +0800

    Update 00.MetaPlot_forAllHelicases.XJH.r

--- 00.MetaPlot_forAllHelicases.XJH.r
+++ 00.MetaPlot_forAllHelicases.XJH.r
@@ -1,5 +1,4 @@
 library(dplyr)
-#install.packages("caroline")
 library(caroline)
 library(tidyr)
 library(dplyr)
@@ -9,9 +8,7 @@ library(VennDiagram)
 library(tidyverse)
 library(export)
 
-#DDX6的路径
 setwd("C:\\Users\\CSIV149\\PLC.lab.docs\\03.larry\\eCLIP_analysis.RX.MetaPlot\\Larry_DDX6")
-#另外三个样本的路径
 setwd("C:\\Users\\CSIV149\\PLC.lab.docs\\03.larry\\eCLIP_analysis.RX.MetaPlot\\Larry_DDX21")
 setwd("C:\\Users\\CSIV149\\PLC.lab.docs\\03.larry\\eCLIP_analysis.RX.MetaPlot\\Larry_DHX9")
 setwd("C:\\Users\\CSIV149\\PLC.lab.docs\\03.larry\\eCLIP_analysis.RX.MetaPlot\\Larry_DDX3X")
@@ -49,22 +46,6 @@ pt = ggplot(data=total, aes(x=num, y=n.ratio, group=1)) +
 pt + 
   geom_vline(xintercept = 12, linetype="dashed", color="grey18", size=1.2) + 
   geom_vline(xintercept = 113, linetype="dashed", color="grey18", size=1.2)
-
-graph2ppt(file="MetaPlot_eCLIP_peaks_DDX6_IgG.pptx", width=8, aspectr=1.5)
-graph2ppt(file="MetaPlot_eCLIP_peaks_DDX6_XL1.pptx", width=8, aspectr=1.5)
-graph2ppt(file="MetaPlot_eCLIP_peaks_DDX6_XL2.pptx", width=8, aspectr=1.5)
-
-graph2ppt(file="MetaPlot_eCLIP_peaks_DDX21_IgG.pptx", width=8, aspectr=1.5)
-graph2ppt(file="MetaPlot_eCLIP_peaks_DDX21_XL1.pptx", width=8, aspectr=1.5)
-graph2ppt(file="MetaPlot_eCLIP_peaks_DDX21_XL2.pptx", width=8, aspectr=1.5)
-
-graph2ppt(file="MetaPlot_eCLIP_peaks_DHX9_IgG.pptx", width=8, aspectr=1.5)
-graph2ppt(file="MetaPlot_eCLIP_peaks_DHX9_XL1.pptx", width=8, aspectr=1.5)
-graph2ppt(file="MetaPlot_eCLIP_peaks_DHX9_XL2.pptx", width=8, aspectr=1.5)
-
-graph2ppt(file="MetaPlot_eCLIP_peaks_DDX3X_IgG.pptx", width=8, aspectr=1.5)
-graph2ppt(file="MetaPlot_eCLIP_peaks_DDX3X_XL1.pptx", width=8, aspectr=1.5)
-graph2ppt(file="MetaPlot_eCLIP_peaks_DDX3X_XL2.pptx", width=8, aspectr=1.5)
 
 
 
